4094699faf8b83a87300fa9fbc0249a4efed2c70
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-11-14 02:24:02 +0700

    membuat navigation drawer

---
+++ app/src/main/java/com/learn/mynavdrawer/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.learn.mynavdrawer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.learn.mynavdrawer.ui.theme.MyNavDrawerTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNavDrawerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   DefaultPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyNavDrawerApp() {
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            MyTopBar(
+                onMenuClick = {
+                    scope.launch {
+                        if (drawerState.isClosed){
+                            drawerState.open()
+                        }else{
+                            drawerState.close()
+                        }
+                    }
+
+                }
+            )
+        },
+    ) {paddingValues ->
+        ModalNavigationDrawer(
+            modifier = Modifier.padding(paddingValues),
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(Modifier.height(12.dp))
+                    Text(stringResource(R.string.hello_from_nav_drawer))
+                }
+            },
+            content = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                  Text(
+                      if(drawerState.isClosed){
+                          stringResource(R.string.swipe_to_open)
+                      }else {
+                          stringResource(R.string.swipe_to_close)
+                      }
+                  )
+                }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(onMenuClick : () -> Unit){
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(stringResource(R.string.app_name))
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    MyNavDrawerTheme {
+        MyNavDrawerApp()
+    }
+}
+
